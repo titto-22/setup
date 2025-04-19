8be716c94dbbaaa55b98eab1694132acaeacc6ed
Vitor Souza <vitorpvp22@gmail.com>
2025-04-18 23:51:05 -0400

    up
    update

--- todos/models.py
+++ todos/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Todo(models.Model):
     title = models.CharField(max_length=100, null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
-    deadline = models.DateTimeField(null=True, blank=True)
-    finished_at = models.BooleanField(null=True, blank=True)
+    deadline = models.DateField(null=True, blank=True)
+    finished_at = models.DateField(null=True,)
